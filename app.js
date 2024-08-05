e5581b703d843a08fd2ff2630f69d3b36a96ff29
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-08-06 04:27:27 +0500

    commands added

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+import { collection, addDoc, getDocs, doc, deleteDoc, updateDoc, Timestamp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js"; 
+
+import { db } from "./config.js";
+
 const form = document.querySelector('#form');
 const inputTitle = document.querySelector('.input-title');
 const inputAmount = document.querySelector('.input-amount');
@@ -5,26 +9,46 @@ const totalAmountWrapper = document.querySelector('.total-amount-2');
 const listWrapper = document.querySelector('.list-wrapper-out');
 const expenseArr = [];
 let totalAmount;
+
+
+
+// value sumbition
 form.addEventListener('submit', event => {
     event.preventDefault();
     addExpense();
-    // localStorage.setItem('localDesc',JSON.stringify(description));
-    // localStorage.setItem('ls',JSON.stringify(description));
 })
 
-function addExpense() {
+
+
+// pushing data to firestore and array
+async function addExpense() {
     let toNumber = Number(inputAmount.value);
-    expenseArr.push({
-        expenseName: inputTitle.value,
-        expenseAmount: toNumber
-    })
-    inputTitle.value= '';
-    inputAmount.value= '';
-    console.log(expenseArr);
-    renderExpense();
-    // totalAmountCalc();
+    try {
+        const docRef = await addDoc(collection(db, "expenses"), {
+            expenseTitle: inputTitle.value,
+            expenseAmount: toNumber,
+            time: Timestamp.fromDate(new Date())
+        });
+        expenseArr.push({
+            expenseName: inputTitle.value,
+            expenseAmount: toNumber,
+            docId : docRef.id
+        })
+        console.log("Document written with ID: ", docRef.id);
+        inputTitle.value= '';
+        inputAmount.value= '';
+        console.log(expenseArr);
+        renderExpense();
+        totalAmountCalc()
+    } catch (e) {
+        console.error("Error adding document: ", e);
+    }
 }
 
+
+
+
+// rendering data on the screen 
 function renderExpense() {
     listWrapper.innerHTML = '';
     for (let i = 0; i < expenseArr.length; i++) {
@@ -32,13 +56,13 @@ function renderExpense() {
         <div class="d-flex justify-content-center list-wrapper-in w-100">
                     <div class="list-style-left">
                     </div>
-                    <div style="width:33%;" class="d-flex align-items-center">
+                    <div style="width: 33%;" class="d-flex align-items-center">
                         <h1 class="m-0 list-name">${expenseArr[i].expenseName}</h1>
                     </div>
-                    <div style="width:33%;" class="d-flex justify-content-center align-items-center">
+                    <div style="width: 33%;" class="d-flex justify-content-center align-items-center">
                         <h2 class="m-0 text-center list-price">$${expenseArr[i].expenseAmount}</h2>
                     </div>
-                    <div style="width:33%;" class="d-flex justify-content-center align-items-center">
+                    <div style="width: 33%;" class="d-flex justify-content-center align-items-center">
                         <div class="d-flex justify-content-center">
                             <i class="fa-solid fa-pen-to-square"></i>
                             <i class="fa-solid fa-trash-can"></i>
@@ -47,11 +71,28 @@ function renderExpense() {
                 </div>
         `
     }
+
+
+
+
+    // edit function
     const editBtn = document.querySelectorAll('.fa-pen-to-square');
     editBtn.forEach((item,index)=>{
         item.addEventListener('click',()=>{
             const edited = prompt('Edit Amount!', expenseArr[index].expenseAmount);
             expenseArr[index].expenseAmount = edited;
+            renderExpense();
+        })
+    })
+
+
+
+
+    // delete function
+    const dltBtn = document.querySelectorAll('.fa-trash-can');
+    dltBtn.forEach((item,index)=>{
+        item.addEventListener('click',()=>{
+            expenseArr.splice(index,1)
             renderExpense();
         })
     })
@@ -70,10 +111,8 @@ function dltExpense(index) {
 }
 
 function totalAmountCalc() {
-    totalAmount = amountArr.reduce((accumulator,currentvalue)=>{
-        return accumulator+currentvalue
-    },0);
-    totalAmountWrapper.innerHTML = `
-    <h1>Total Amount = ${totalAmount}</h1>
-    `
+    totalAmount = expenseArr.reduce((accumulator, currentExpense) => {
+        return accumulator + currentExpense.expenseAmount;
+    }, 0);
+    totalAmountWrapper.innerHTML = `$${totalAmount}`
 }
