a3c5444d6cb423f73164c31a06b4e57275b55db0
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-08-05 18:12:01 +0500

    commands added

--- app.js
+++ app.js
@@ -1,47 +1,52 @@
 const form = document.querySelector('#form');
 const input1 = document.querySelector('#input1');
 const input2 = document.querySelector('#input2');
-const descriptionWrapper = document.querySelector('#description-wrapper');
-const amountWrapper = document.querySelector('#amount-wrapper');
-const buttonsWrapper = document.querySelector('#buttons-wrapper');
-const description = [];
-const amountArr = [];
-const totalAmountWrapper = document.querySelector('#totalamount-wrapper');
+const totalAmountWrapper = document.querySelector('.total-amount-2');
+const listWrapper = document.querySelector('.list-wrapper-out');
+const expenseArr = [];
 let totalAmount;
 form.addEventListener('submit', event => {
     event.preventDefault();
-    console.log(545);
-    addExpense();
-    localStorage.setItem('localDesc',JSON.stringify(description));
-    localStorage.setItem('ls',JSON.stringify(description));
+    console.log(input1.value);
+    console.log(input2.value);
+    // addExpense();
+    // localStorage.setItem('localDesc',JSON.stringify(description));
+    // localStorage.setItem('ls',JSON.stringify(description));
 })
 
 function addExpense() {
-    let toNumber = Number(input2.value);
-    description.push(input1.value);
-    amountArr.push(toNumber);
-    input1.value= '';
-    input2.value= '';
-    renderExpense();
-    totalAmountCalc();
+    console.log(input1.value);
+    console.log(input2.value);
+    // let toNumber = Number(input2.value);
+    // expenseArr.push({
+    //     expenseName: input1.value,
+    //     expenseAmount: toNumber
+    // })
+    // input1.value= '';
+    // input2.value= '';
+    // renderExpense();
+    // totalAmountCalc();
 }
 
 function renderExpense() {
-    descriptionWrapper.innerHTML = '<h2>Description</h2>';
-    amountWrapper.innerHTML = '<h2>Amount</h2>';
-    buttonsWrapper.innerHTML = '<h2>Edit/Dlt</h2>';
-    for (let i = 0; i < description.length; i++) {
-        descriptionWrapper.innerHTML += `
-        <p>${description[i]}</p>
-        `
-        amountWrapper.innerHTML += `
-        <p>${amountArr[i]}</p>
-        `
-        buttonsWrapper.innerHTML += `
-        <p>
-        <button onclick="editExpense(${i})">Edit</button>
-        <button onclick="dltExpense(${i})">Delete</button>
-        </p>
+    for (let i = 0; i < expenseArr.length; i++) {
+        listWrapper.innerHTML += `
+        <div class="d-flex justify-content-center list-wrapper-in w-100">
+                    <div class="list-style-left">
+                    </div>
+                    <div style="flex-grow: 1;" class="d-flex align-items-center">
+                        <h1 class="m-0 list-name">Food</h1>
+                    </div>
+                    <div style="flex-grow: 1;" class="d-flex justify-content-center align-items-center">
+                        <h2 class="m-0 text-center list-price">$50</h2>
+                    </div>
+                    <div style="flex-grow: 1;" class="d-flex justify-content-center align-items-center">
+                        <div class="d-flex justify-content-center">
+                            <i class="fa-solid fa-pen-to-square"></i>
+                            <i class="fa-solid fa-trash-can"></i>
+                        </div>
+                    </div>
+                </div>
         `
     }
 }
